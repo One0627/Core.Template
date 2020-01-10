e94aefa4d322f558618f02c54a906825c785dade
songlun <79060087@qq.com>
2020-01-10 17:52:44 +0800

    1

--- Core.Template.Api/Program.cs
+++ Core.Template.Api/Program.cs
@@ -14,11 +14,9 @@ namespace Core.Template.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = WebHost.CreateDefaultBuilder(args).UseUrls("http://*:5000").UseStartup<Startup>().Build();
+            host.Run();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
+
